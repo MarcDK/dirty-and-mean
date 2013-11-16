41e183a5d4c32c35894a9ac7a79d80eee023e073
Marc Tönsing <marc@eatsmarter.de>
2013-11-16 19:15:59 +0100

    Fixed missing parameter and fixed middle style

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <?php echo get_marctv_sticky_posts(); ?>
 
     <?php echo get_marctv_category_container(get_option("marctv_cat1"), get_option("marctv_cat2"), get_option("marctv_cat3"), FALSE, 'docked'); ?>
-    <?php echo get_marctv_category_container(get_option("marctv_cat1"), get_option("marctv_cat2"), get_option("marctv_cat3"), '0', 'docked'); ?>
+    <?php echo get_marctv_category_container(get_option("marctv_cat1"), get_option("marctv_cat2"), get_option("marctv_cat3"), '', 'docked'); ?>
 
     <ul class="container bars">
       <li class="box first cat-more"><a href="<?php echo get_category_link(get_option("marctv_cat1")) ?>">Leben</a></li>
