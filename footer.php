12c97306a2d9547bf1d485f006f08db9f8338f9a
Marc Tönsing <marc@eatsmarter.de>
2014-01-19 23:07:35 +0100

    removed underline

--- footer.php
+++ footer.php
@@ -30,52 +30,54 @@
 </div> <!-- /site -->
 <div id="bottom">
   <div class="inner">
-    <ul class="container section">
-      <li class="box first">
-        <div class="supertitle">Suche</div>
-<?php get_search_form(); ?>
-      </li>
-      <li class="box">
-        <div class="supertitle">Abonnieren</div>
-        <ul class="rows">
-          <li>
-            <ul>
-              <li><a rel="nofollow" target="_blank" href="https://www.facebook.com/MarcTV.de">Facebook</a></li>
-              <li><a rel="nofollow" target="_blank" href="https://twitter.com/marctv">Twitter</a></li>
-              <li><a rel="nofollow" target="_blank" href=" http://cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fmarctv.de%2Ffeed">Feedly</a></li>
-            </ul>
-          </li>
-          <li>
-            <ul>
-              <li><a rel="nofollow" target="_blank" href="http://www.youtube.com/MarcDK">YouTube</a></li>
-              <li><a rel="nofollow" target="_blank" href="https://plus.google.com/+MarcTönsing?rel=author">Google+</a></li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-      <li class="box last">
-        <div class="supertitle">Rechtliches</div>
-        <ul class="rows">
-          <li>
-            <ul>
-              <li><a href="http://marctv.de/marc-toensing/#impressum">Impressum</a></li>
-            </ul>
-          </li>
-        </ul>
-      </li>
+    <nav>
+      <ul class="container section">
+        <li class="box first">
+          <div class="supertitle">Suche</div>
+          <?php get_search_form(); ?>
+        </li>
+        <li class="box">
+          <div class="supertitle">Abonnieren</div>
+          <ul class="rows">
+            <li>
+              <ul>
+                <li><a rel="nofollow" target="_blank" href="https://www.facebook.com/MarcTV.de">Facebook</a></li>
+                <li><a rel="nofollow" target="_blank" href="https://twitter.com/marctv">Twitter</a></li>
+                <li><a rel="nofollow" target="_blank" href=" http://cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fmarctv.de%2Ffeed">Feedly</a></li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li><a rel="nofollow" target="_blank" href="http://www.youtube.com/MarcDK">YouTube</a></li>
+                <li><a rel="nofollow" target="_blank" href="https://twitch.tv/marctvde">Twitch.TV</a></li>
+                <li><a rel="nofollow" target="_blank" href="https://plus.google.com/+MarcTönsing?rel=author">Google+</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li class="box last">
+          <div class="supertitle">Rechtliches</div>
+          <ul class="rows">
+            <li>
+              <ul>
+                <li><a href="http://marctv.de/marc-toensing/#impressum">Impressum</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
 
-    </ul>
-
+      </ul>
+    </nav>
   </div>
-<?php
-wp_reset_query();
-?>
+  <?php
+  wp_reset_query();
+  ?>
   <div class="lastwords">Dirty and Mean - MarcTV <?php echo date('Y'); ?></div>
 </div>
 
 </body>
 </html>
 <!-- <?php
-  printf(__('%d queries. %s seconds. '), get_num_queries(), timer_stop(0, 3));
-  echo_memory_usage();
+printf(__('%d queries. %s seconds. '), get_num_queries(), timer_stop(0, 3));
+echo_memory_usage();
 ?> -->
