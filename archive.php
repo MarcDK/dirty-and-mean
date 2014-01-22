ce2f0b24652e7ef27884aa2007fd5d12d0cc8982
Marc Tönsing <marc@eatsmarter.de>
2014-01-22 21:20:19 +0100

    fixed syntax error.

--- archive.php
+++ archive.php
@@ -65,57 +65,59 @@ get_header();
     </div>
 
     <ul class="container morph">
-  <?php
-  $key = 0;
-  while (have_posts()) : the_post();
-    $key++;
-    // cfvalue:field 
-   if ($key % 6 == 0) {
-        echo '<li class="box last">';
-      }
-      else if (($key - 1) % 6 == 0) {
-        echo '<li class="box first">';
-      }
-      else if (($key - 4 ) % 6 == 0) {
-        echo '<li class="box multi-first">';
-      }
-      else if (($key - 3 ) % 6 == 0) {
-        echo '<li class="box multi-last">';
-      }
-      else {
-        echo '<li class="box">';
-      }
-
-    echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', true);
-
-    if ($key % 6 == 0) {
-      echo '</ul>';
-    }
-    ?>
-        </li>
-      <?php endwhile; ?>
-   
-
       <?php
-    else :
-      if (is_category()) { // If this is a category archive
-        printf("<h1 class='center'>Sorry, but there aren't any posts in the %s category yet.</h1>", single_cat_title('', false));
-      }
-      else if (is_date()) { // If this is a date archive
-        echo("<h1>Sorry, but there aren't any posts with this date.</h1>");
-      }
-      else if (is_author()) { // If this is a category archive
-        $userdata = get_userdatabylogin(get_query_var('author_name'));
-        printf("<h1 class='center'>Sorry, but there aren't any posts by %s yet.</h1>", $userdata->display_name);
-      }
-      else {
-        echo("<h1 class='center'>No posts found.</h1>");
-      }
-      get_search_form();
-    endif;
-    ?>
+      $key = 0;
+      while (have_posts()) : the_post();
+        $key++;
+        // cfvalue:field 
+        if ($key % 6 == 0) {
+          echo '<li class="box last">';
+        }
+        else if (($key - 1) % 6 == 0) {
+          echo '<li class="box first">';
+        }
+        else if (($key - 4 ) % 6 == 0) {
+          echo '<li class="box multi-first">';
+        }
+        else if (($key - 3 ) % 6 == 0) {
+          echo '<li class="box multi-last">';
+        }
+        else {
+          echo '<li class="box">';
+        }
 
-    </ul>
+        echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', true);
+        ?>
+        </li>
+        <?php
+        if ($key % 6 == 0) {
+          echo '</ul>';
+        }
+
+      endwhile;
+      ?>
+
+
+  <?php
+else :
+  if (is_category()) { // If this is a category archive
+    printf("<h1 class='center'>Sorry, but there aren't any posts in the %s category yet.</h1>", single_cat_title('', false));
+  }
+  else if (is_date()) { // If this is a date archive
+    echo("<h1>Sorry, but there aren't any posts with this date.</h1>");
+  }
+  else if (is_author()) { // If this is a category archive
+    $userdata = get_userdatabylogin(get_query_var('author_name'));
+    printf("<h1 class='center'>Sorry, but there aren't any posts by %s yet.</h1>", $userdata->display_name);
+  }
+  else {
+    echo("<h1 class='center'>No posts found.</h1>");
+  }
+  get_search_form();
+endif;
+?>
+
+ 
   <div class="nav-article">
     <span class="nav-previous"><?php echo get_previous_posts_link('« Vorherige'); ?>&nbsp;</span>
     <span class="nav-next">&nbsp;<?php echo get_next_posts_link('Nächste »'); ?></span>
