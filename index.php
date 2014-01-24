a98fc425e6d2dcd0430e393177b4e17ffa9da551
Marc Tönsing <marc@eatsmarter.de>
2014-01-24 17:05:04 +0100

    Added paged homepage.

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
         <?php echo get_marctv_teaser($lead_pid, true, '', 'default'); ?>
       </li>
     </ul>
-    
+
     <?php echo get_marctv_last_commented_articles(); ?>
-    
+
     <?php echo get_marctv_category_container(get_option("marctv_cat1"), get_option("marctv_cat2"), get_option("marctv_cat3"), FALSE, 'docked'); ?>
 
 
@@ -40,7 +40,7 @@
       <li class="box last cat-more"><a href="<?php echo get_category_link(get_option("marctv_cat3")) ?>">Medien</a></li>
     </ul>
 
-    
+
     <?php //echo get_marctv_sticky_posts(); ?>
 
     <div id="marctvflickrbar"></div>
@@ -48,12 +48,54 @@
     <?php echo get_marctv_favourite_articles(); ?>
 
     <?php echo get_marctv_most_commented_articles(); ?>
-    
+
     <?php echo marctv_get_randompost(); ?>
-   
+
   </div>
   <?php wp_reset_query(); ?>
 <?php else : ?>
-  <?php wp_redirect("http://marctv.de/paged", 404) ?>
+  <div id="content" class="section">
+    <?php
+    if (function_exists('yoast_breadcrumb')) {
+      yoast_breadcrumb('<div class="section breadcrumbs">', '</div>');
+    }
+    ?>
+    <div class="entry">
+      <h1 class="title">Blog Archiv</h1>
+    </div>
+    <ul class="container morph">
+      <?php
+      $key = 0;
+      while (have_posts()) : the_post();
+        $key++;
+        // cfvalue:field 
+        if ($key % 6 == 0) {
+          echo '<li class="box last">';
+        } else if (($key - 1) % 6 == 0) {
+          echo '<li class="box first">';
+        } else if (($key - 4 ) % 6 == 0) {
+          echo '<li class="box multi-first">';
+        } else if (($key - 3 ) % 6 == 0) {
+          echo '<li class="box multi-last">';
+        } else {
+          echo '<li class="box">';
+        }
+
+        echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', true);
+
+        echo '</li>';
+
+      endwhile;
+
+      echo '</ul>';
+      ?>
+
+      <div class="nav-article">
+        <span class="nav-previous"><?php echo get_previous_posts_link('« Vorherige'); ?>&nbsp;</span>
+        <span class="nav-next">&nbsp;<?php echo get_next_posts_link('Nächste »'); ?></span>
+      </div>
+
+      <?php marctv_pagination(" ", '<div class="nav-paged">', "</div>", "« Vorherige", "Nächste »", 'span', '6'); ?>
+  </div>
 <?php endif; ?>
 <?php get_footer(); ?>
