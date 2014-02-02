4d8e07b911deb0056f699a79ecc0cf7702586a83
Marc Tönsing <marc@eatsmarter.de>
2014-02-02 10:13:14 +0100

    Changed HEader color to #fff. less is more.

--- js/marctv_base.js
+++ js/marctv_base.js
@@ -16,7 +16,7 @@
 
     if (is_touch_device()) {
       $('body').addClass('is-touch');
-    }else{
+    } else {
       $(".header .innerheader").sticky({
         topSpacing: 0,
         redocked_callback: redocked,
@@ -25,20 +25,21 @@
     }
     // detect touch device and adds body class
     function is_touch_device() {
-      return 'ontouchstart' in window // works on most browsers 
+      return 'ontouchstart' in window // works on most browsers
               || 'onmsgesturechange' in window; // works on ie10
     }
 
     function undocked(el) {
-     
+
       /* el.css('background', $('body').css('background-color')).css('box-shadow', '0 2px 6px #333'); */
     }
 
     function redocked(el) {
-      
-     /* el.css('background', 'transparent').css('box-shadow', 'none'); */
+
+      /* el.css('background', 'transparent').css('box-shadow', 'none'); */
     }
 
 
+
   });
-})(jQuery); 
+})(jQuery);
