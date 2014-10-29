f93db2a79f8b458c9462d4942cd4e40159cb5988
Marc Tönsing <marc@eatsmarter.de>
2014-10-29 16:18:52 -0700

    Changed a few things for wpseo.

--- header.php
+++ header.php
@@ -19,13 +19,13 @@
       wp_title('-', true, 'right');
 
       // Add the blog name.
-      bloginfo('name');
+      
 
       // Add the blog description for the home/front page.
-      $site_description = get_bloginfo('description', 'display');
+     /* $site_description = get_bloginfo('description', 'display');
       if ($site_description && ( is_home() || is_front_page() ))
         echo " - $site_description";
-
+      */
       // Add a page number if necessary:
       if ($paged >= 2 || $page >= 2)
         echo ' - ' . sprintf(__('Seite %s'), max($paged, $page));
