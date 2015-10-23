01ef56637b6652c44c7ac76424cec07dc2e717ad
Marc Tönsing <marc@eatsmarter.de>
2015-10-23 21:04:47 +0200

    marctv to marc.tv

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
   <div class="entry">
     <h1 class="">404 - Not Found</h1>
-    <p><img src="http://marctv.de/media/4044.gif" alt="Sorry Mario but your page is in another castle" /></p>
+    <p><img src="/media/4044.gif" alt="Sorry Mario but your page is in another castle" /></p>
     <p style="font-size: 200%;">Nichts gefunden. <a href="<?php echo site_url(); ?>">Zur Startseite?</a>  </p>
   </div>
 </div>
