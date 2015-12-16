47c99e8b98778578cd1b0f6c89f17cfd0ba026e7
Marc Tönsing <marc@eatsmarter.de>
2015-12-16 19:06:49 +0100

    dark theme

--- header.php
+++ header.php
@@ -1,40 +1,54 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="de-DE" id="marctv">
-  <!--
+<!--
 
-  Grüße an alle Quelltextleser!
+Grüße an alle Quelltextleser!
 
-  - Marc
+- Marc
 
-  -->
-  <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-      <?php if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); ?>
-      <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-      <?php wp_enqueue_style('style', get_stylesheet_uri()); ?>
-      <?php wp_head(); ?>
-      <meta name="viewport" content="user-scalable=1, width=device-width, initial-scale=1, maximum-scale=1">
-      <link href='http://fonts.googleapis.com/css?family=Noto Serif:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
-      <link href='http://fonts.googleapis.com/css?family=Noto Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
-  </head>
+-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <?php if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); ?>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+    <?php wp_enqueue_style('style', get_stylesheet_uri()); ?>
+    <?php wp_head(); ?>
+    <meta name="viewport" content="user-scalable=1, width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Noto Serif:400italic,700italic,400,700' rel='stylesheet'
+          type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Noto Sans:400italic,700italic,400,700' rel='stylesheet'
+          type='text/css'>
+</head>
 
-  <body <?php body_class(); ?>>
-    <div id=fig1><div class="figure"><div></div></div></div>
-    <div id=fig2><div class="figure"><div></div></div></div>
-    <div class="header">
-      <div id="header" class="innerheader">
-        <div class="site">
-          <div class="section">
-            <?php if (is_home()) : ?>
-              <h1 class="sitelogo"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
-            <?php else : ?>
-              <div class="sitelogo"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></div>
-            <?php endif ?>
-            <nav id="primary-navigation"><div id="navigation"><?php wp_nav_menu(array('theme_location' => 'mainnav', 'container' => '')); ?></div></nav>
-            <?php get_search_form(); ?>
-          </div>
-        </div>
-      </div>
+<body <?php body_class(); ?>>
+<div id=fig1>
+    <div class="figure">
+        <div></div>
     </div>
-    <div class="site main-content">
+</div>
+<div id=fig2>
+    <div class="figure">
+        <div></div>
+    </div>
+</div>
+<div class="header">
+    <div id="header" class="innerheader">
+        <div class="site">
+            <div class="section">
+                <?php if (is_home()) : ?>
+                    <h1 class="sitelogo"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
+                <?php else : ?>
+                    <div class="sitelogo"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a>
+                    </div>
+                <?php endif ?>
+                <nav id="primary-navigation">
+                    <div
+                        id="navigation"><?php wp_nav_menu(array('theme_location' => 'mainnav', 'container' => '')); ?></div>
+                </nav>
+                <?php get_search_form(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="site main-content">
 
--- search.php
+++ search.php
@@ -9,49 +9,52 @@
 get_header();
 ?>
 
-<div id="content" class="section">
+    <div id="content" class="section">
 
-  <div class="entry">
-    <h1 class="title">Suchergebnisse für '<span><?php echo get_search_query(); ?></span>'</h1>
-  </div>
-  <ul class="container oneline">
-    <?php if (have_posts()) : ?>
-      <?php
-      $key = 0;
-      while (have_posts()) : the_post();
-        $key++;
-        // cfvalue:field 
-        if ($key % 3 == 0) {
-          echo '<li class="box last">';
-        }
-        else if (($key - 1) % 3 == 0) {
-          echo '<li class="box first">';
-        }
-        else {
-          echo '<li class="box">';
-        }
+        <div class="entry">
+            <h1 class="title">Suchergebnisse für '<span><?php echo get_search_query(); ?></span>'</h1>
+        </div>
 
-        echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', false);
+        <?php if (have_posts()) : ?>
 
-        if ($key % 3 == 0) {
-          echo '</ul><ul class="container">';
-        }
-        ?>
-        </li>
-      <?php endwhile; ?>
-    </ul>
-  <?php else : ?>
+        <ul class="container six docked">
 
-    <article id="post-0" class="post no-results not-found">
-      <h1 class="entry-title">Nichts gefunden</h1>
-      <div class="entry-content">
-        <p>Entschuldige, aber es konnte nichts gefunden werden. Versuche es mit anderen Schlüsselwörtern erneut.</p>
-        <?php get_search_form(); ?>
-      </div><!-- .entry-content -->
-    </article><!-- #post-0 -->
+            <?php
+            $key = 0;
+            while (have_posts()) : the_post();
+                $key++;
+                // cfvalue:field
+                if ($key == 1) {
+                    echo '<li class="box first">';
+                } else if ($key % 6 == 0) {
+                    echo '<li class="box last">';
+                } else if ($key == 3) {
+                    echo '<li class="box multi-last">';
+                } else if ($key == 4) {
+                    echo '<li class="box multi-first">';
+                } else {
+                    echo '<li class="box">';
+                }
 
-  <?php endif; ?>
+                echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', true);
 
-  <?php //marctv_pagination(" ", '<div class="nav-paged">', "</div>", "« Vorherige", "Nächste »", 'span', '6'); ?>
-</div>
+                echo '</li>';
+            endwhile;
+
+            else : ?>
+
+                <article id="post-0" class="post no-results not-found">
+                    <h1 class="entry-title">Nichts gefunden</h1>
+
+                    <div class="entry-content">
+                        <p>Entschuldige, aber es konnte nichts gefunden werden. Versuche es mit anderen Schlüsselwörtern
+                            erneut.</p>
+                        <?php get_search_form(); ?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-0 -->
+
+            <?php endif; ?>
+
+            <?php //marctv_pagination(" ", '<div class="nav-paged">', "</div>", "« Vorherige", "Nächste »", 'span', '6'); ?>
+    </div>
 <?php get_footer(); ?>
