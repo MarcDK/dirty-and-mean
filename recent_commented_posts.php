8b8107c3f72978b8b7b32be992b967548830ee70
Marc Tönsing <marc.toensing@gmail.com>
2016-10-12 12:30:03 +0200

    Fixed margins on page templates

--- recent_commented_posts.php
+++ recent_commented_posts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Recent Commented
+ * Template Name: Letzte Kommentare
  *
  * Description: A custom top game
  *
@@ -8,34 +8,33 @@
 get_header();
 ?>
 
+<div class="site ">
+    <div id="content" class="section">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="entry">
+                <h1 class="title entry-title">
+                    <span><?php esc_html(the_title()); ?><?php edit_post_link('edit', '<small> ', '</small>'); ?></span>
+                </h1>
+                <?php the_content(); ?>
+            </div>
+            <?php
+        endwhile;
+        endif;
 
-<div id="content" class="section">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <div class="entry">
-        <h1 class="title entry-title"><span><?php esc_html(the_title()); ?><?php edit_post_link('edit', '<small> ', '</small>'); ?></span></h1>
-            <?php the_content(); ?>
-      </div>
-      <?php
-    endwhile;
-  endif;
+        if (function_exists('get_last_commented_articles')) {
+            //echo '<div class="container docked"><div class="supertitle"><span><a  href="/letzte-kommentare">Zuletzt kommentiert</a></span></div></div>';
+            echo get_last_commented_articles(18, 'container last-commented multi nohover showontouch');
+        }
 
 
+        /* Restore original Post Data
+         * NB: Because we are using new WP_Query we aren't stomping on the
+         * original $wp_query and it does not need to be reset.
+         */
+        wp_reset_postdata();
+        ?>
 
-    if (function_exists('get_last_commented_articles')) {
-      echo '<div class="container docked"><div class="supertitle"><span><a  href="/letzte-kommentare">Zuletzt kommentiert</a></span></div></div>';
-      echo get_last_commented_articles(18, 'container last-commented multi nohover showontouch');
-    }
-
-
-  /* Restore original Post Data
-   * NB: Because we are using new WP_Query we aren't stomping on the
-   * original $wp_query and it does not need to be reset.
-   */
-  wp_reset_postdata();
-  ?>
-</ul>
-<?php marctv_pagination(" ", '<span class="pagenav">', "</span>", "« Vorherige", "Nächste »", 'span', '9'); ?>
-</div><!-- #primary -->
-
+    </div><!-- #primary -->
+</div>
 <?php get_footer(); ?>
 
--- sortbyreview.php
+++ sortbyreview.php
@@ -1,81 +1,81 @@
 <?php
 /**
- * Template Name: Top Games
+ * Template Name: Spiele Top 100
  *
  * Description: A custom top game
  *
  */
 get_header();
 ?>
-<div class="site main-content">
-<div id="content" class="section">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <div class="entry">
-        <h1 class="title entry-title"><span><?php esc_html(the_title()); ?><?php edit_post_link('edit', '<small> ', '</small>'); ?></span></h1>
-        <?php the_content(); ?>
-      </div>
-      <?php
-    endwhile;
-  endif;
-  ?>
+<div class="site">
+    <div id="content" class="section">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="entry">
+                <h1 class="title entry-title"><span><?php esc_html(the_title()); ?><?php edit_post_link('edit', '<small> ', '</small>'); ?></span></h1>
+                <?php the_content(); ?>
+            </div>
+            <?php
+        endwhile;
+        endif;
+        ?>
 
-  <ul class="container morph showontouch docked">
-    <?php
-    $args = array(
-      'post_type' => 'post',
-      'meta_key' => "_shortscore_user_rating",
-      'orderby' => 'meta_value_num',
-      'posts_per_page' => '200',
-      'cat' => get_option("marctv_cat2"),
-      'nopaging' => false,
-      'order' => 'DESC'
-    );
+        <ul class="container morph nohover docked">
+            <?php
+            $args = array(
+                'post_type' => 'post',
+                'meta_key' => "_shortscore_user_rating",
+                'orderby' => 'meta_value_num',
+                'posts_per_page' => '200',
+                'cat' => get_option("marctv_cat2"),
+                'nopaging' => false,
+                'order' => 'DESC'
+            );
 
-    $the_query = new WP_Query($args);
+            $the_query = new WP_Query($args);
 
-    $key = 0;
-    
-    
-    while ($the_query->have_posts()) :
-      $the_query->the_post();
-      $key++;
-      // cfvalue:field 
-      if ($key % 6 == 0) {
-        echo '<li class="box last">';
-      }
-      else if (($key - 1) % 6 == 0) {
-        echo '<li class="box first">';
-      }
-      else if (($key - 4 ) % 6 == 0) {
-        echo '<li class="box multi-first">';
-      }
-      else if (($key - 3 ) % 6 == 0) {
-        echo '<li class="box multi-last">';
-      }
-      else {
-        echo '<li class="box">';
-      }
+            $key = 0;
 
-      echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', false);
 
-      if ($key % 6 == 0) {
-        echo '</ul><ul class="container morph showontouch docked">';
-      }
-      ?>
-      </li>
-    <?php endwhile; ?>
-  </ul>
+            while ($the_query->have_posts()) :
+                $the_query->the_post();
+                $key++;
+                // cfvalue:field
+                if ($key % 6 == 0) {
+                    echo '<li class="box last">';
+                }
+                else if (($key - 1) % 6 == 0) {
+                    echo '<li class="box first">';
+                }
+                else if (($key - 4 ) % 6 == 0) {
+                    echo '<li class="box multi-first">';
+                }
+                else if (($key - 3 ) % 6 == 0) {
+                    echo '<li class="box multi-last">';
+                }
+                else {
+                    echo '<li class="box">';
+                }
 
-  <?php
-  /* Restore original Post Data 
-   * NB: Because we are using new WP_Query we aren't stomping on the 
-   * original $wp_query and it does not need to be reset.
-   */
-  wp_reset_postdata();
-  ?>
-</ul>
-<?php marctv_pagination(" ", '<span class="pagenav">', "</span>", "« Vorherige", "Nächste »", 'span', '9'); ?>
-</div><!-- #primary -->
+                echo get_marctv_teaser(get_the_ID(), true, '', 'medium', true, '', '', false);
+
+                if ($key % 6 == 0) {
+                    echo '</ul><ul class="container morph nohover docked">';
+                }
+                ?>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+
+        <?php
+        /* Restore original Post Data
+         * NB: Because we are using new WP_Query we aren't stomping on the
+         * original $wp_query and it does not need to be reset.
+         */
+        wp_reset_postdata();
+        ?>
+        </ul>
+        <?php marctv_pagination(" ", '<span class="pagenav">', "</span>", "« Vorherige", "Nächste »", 'span', '9'); ?>
+    </div><!-- #primary -->
 </div>
 
 <?php get_footer(); ?>
